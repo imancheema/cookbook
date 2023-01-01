e3a8010145928be2d3bb92fa5dc7910b1933cdbb
iman cheema <cheema.iman2@gmail.com>
2023-01-01 14:39:13 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Landing from './screens/Landing';
+import Searched from './screens/Searched'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router> 
+      <div class="App">
+        <Routes>  //allows us to store the different urls in the application
+          //the route renders some UI - or screen - when its path matches the current url
+          <Route path='/' element={<Landing/>}/> //landing screen
+          <Route path='/recipe/:term' element={<Searched/>}/> //search screen - colon term is placeholder and any word can go there
+          </Routes>
+      </div>
+    </Router>
   );
 }
 
